d27929432ee750f73bff4c76a598f59d04115d58
Coldmizu <1339447407@qq.com>
2019-10-09 21:13:52 +0800

    commitdemo2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {app,BrowserWindow}=require('electron');
 function createWindow(){
-    var win=new BrowserWindow({width:800,height:600});
+    var win=new BrowserWindow({width:800,height:600,minWidth:200,minHeight:400,
+    x:10,y:10});
     win.loadFile('index.html');
     win.on('closed',()=>{
         console.log('closed');
