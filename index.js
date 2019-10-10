6e138d3aa6b758afc9b51f63b40a9d6b422f3f66
Coldmizu <1339447407@qq.com>
2019-10-10 22:10:30 +0800

    demostorm

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const {app,BrowserWindow}=require('electron');
 function createWindow(){
-    var win=new BrowserWindow({width:800,height:600,minWidth:200,minHeight:400,
-    x:10,y:10});
+    var win=new BrowserWindow({fullscreen:false,show:false});
+    
+    //更改程序icon
+    win.setIcon('images\\icon.ico')
+    //优雅得载入页面
+    //ready-to-show事件
+    win.on('ready-to-show',()=>{
+        win.show();
+    })
+    //子窗口使用模态窗口modal:true
+    //var childwin=new BrowserWindow({parent:win,modal:true});
+
     win.loadFile('index.html');
+    //childwin.loadFile('childindex.html')
     win.on('closed',()=>{
         console.log('closed');
-        win=null;
+        win=null; 
     })
 }
 
